b1d54a870cee774461087fe0c29bc2bb39ec099f
Vic <syuan0212@gmail.com>
2022-05-04 16:55:37 +0800

    fix image api response data

--- controllers/image.js
+++ controllers/image.js
@@ -8,11 +8,17 @@ const image = {
   async uploadToImgur(req, res) {
     try {
       const client = new ImgurClient({ clientId: process.env.CLIENT_ID });
+      console.log(req.file);
       const response = await client.upload({
         image: fs.createReadStream(req.file.path),
+        name: req.file.filename,
         type: 'stream',
       });
-      handleSuccess(res, response.data.link)
+      const responseData = {
+        link: response.data.link,
+        name: response.data.name
+      }
+      handleSuccess(res, responseData)
     } catch {
       handleError(res, 400, 40001)
     }
