d64453022b556dffe9120000d210e93c3bbf9b30
Vic <syuan0212@gmail.com>
2022-05-09 17:15:37 +0800

    update user model

--- model/user.js
+++ model/user.js
@@ -3,11 +3,11 @@ const userScheam = new Schema(
   {
     name: {
       type: String,
-      required: [true, "{PATH} 為必要欄位"]
+      required: [true, '{PATH} 為必要欄位']
     },
     email: {
       type: String,
-      required: [true, "{PATH} 為必要欄位"],
+      required: [true, '{PATH} 為必要欄位'],
       unique: true,
       lowercase: true,
       select: false
@@ -16,6 +16,16 @@ const userScheam = new Schema(
       type: String,
       default: "https://fakeimg.pl/50x50/",
     },
+    sex: {
+      type: String,
+      enum: ['male', 'female']
+    },
+    password: {
+      type: String,
+      required: [true, '{PATH} 為必要欄位'],
+      minlength: 8,
+      select: false
+    }
   },
   {
     versionKey: false
