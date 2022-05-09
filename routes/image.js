f38215eae30638d75dbbe3d39289c4c1c362ff00
Vic <syuan0212@gmail.com>
2022-05-09 13:45:59 +0800

    fix ;

--- routes/image.js
+++ routes/image.js
@@ -4,7 +4,7 @@ const ImageControllers = require('../controllers/image');
 const HttpControllers = require('../controllers/http');
 const handleErrorAsync = require("../service/handleErrorAsync");
 const multer = require('multer');
-const upload = multer({dest: './uploads/'})
+const upload = multer({dest: './uploads/'});
 
 router.post('/', upload.single('image'), handleErrorAsync((req, res, next) => ImageControllers.uploadToImgur(req, res, next)));
 router.options('/', (req, res) => HttpControllers.cors(req, res));
