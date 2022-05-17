a37b33794afc892f3c06c3db637d34869c1c8035
Vic <syuan0212@gmail.com>
2022-05-17 22:49:03 +0800

    fix: cors exposedHeaders

--- routes/user.js
+++ routes/user.js
@@ -4,12 +4,12 @@ const UserControllers = require('../controllers/user');
 const HttpControllers = require('../controllers/http');
 const handleErrorAsync = require("../service/handleErrorAsync");
 const { isAuth } = require('../service/auth');
+const cors = require('cors');
 
-
-router.get('/check', isAuth, handleErrorAsync((req, res, next) => UserControllers.check(req, res, next)));
+router.get('/check', cors({ exposedHeaders: 'Authorization' }), isAuth, handleErrorAsync((req, res, next) => UserControllers.check(req, res, next)));
 router.get('/profile', isAuth, handleErrorAsync((req, res, next) => UserControllers.profile(req, res, next)));
 router.post('/signup', handleErrorAsync((req, res, next) => UserControllers.signup(req, res, next)));
-router.post('/signin', handleErrorAsync((req, res, next) => UserControllers.signin(req, res, next)));
+router.post('/signin', cors({ exposedHeaders: 'Authorization' }), handleErrorAsync((req, res, next) => UserControllers.signin(req, res, next)));
 router.post('/updatePassword', isAuth, handleErrorAsync((req, res, next) => UserControllers.updatePassword(req, res, next)));
 router.patch('/profile', isAuth, handleErrorAsync((req, res, next) => UserControllers.updateProfile(req, res, next)));
 
