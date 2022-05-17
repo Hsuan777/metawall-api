c71de77e254f304ad878e689d254ba71c37e4d8a
Vic <syuan0212@gmail.com>
2022-05-17 21:43:16 +0800

    fix: cors exposeHeaders

--- app.js
+++ app.js
@@ -30,7 +30,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({
+  exposedHeaders: 'Authorization'
+}));
 
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
