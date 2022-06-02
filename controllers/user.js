110230f3e3cc502f6aaad412745e44ba47e2e2ab
Vic <syuan0212@gmail.com>
2022-06-02 16:31:28 +0800

    fix: imgur await axios and postPosts

--- controllers/user.js
+++ controllers/user.js
@@ -113,7 +113,7 @@ const user = {
       return appError(40003, next, '請選擇性別或不公開');
     }
     if (req.files.length === 1) {
-      userAvatar = await Imgur.upload(req.files, 107, 107);
+      userAvatar = await Imgur.upload(req.files, next, 107, 107);
       data.avatar = userAvatar[0].url;
     }
     const result = await User.findByIdAndUpdate(req.user.id, {
